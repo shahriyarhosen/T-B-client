4dd093e99df8cc0f271e30711bad3661c268356c
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-08-01 16:49:36 +0600

    route setup done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
+// Import Routes & Route
 import { Route, Routes } from "react-router-dom";
+
+// Import Components
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+// Import Pages
+import Home from "./pages/Home";
+import PropertyDetails from "./pages/PropertyDetails";
 
 const App = () => {
   return (
-    <section>
+    <section className="max-w-[1440px] mx-auto bg-white">
+      <Header />
       <Routes>
-        <Route to="" element=""></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/property/:id" element={<PropertyDetails />} />
       </Routes>
-      <h1 className="text-7xl text-primary">React app</h1>
+      <Footer />
     </section>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// Import Router
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
