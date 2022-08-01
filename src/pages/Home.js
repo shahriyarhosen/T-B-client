0ba8bda391a9a1e5684cb52fa0f3fc6685e2cfc6
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-08-01 22:11:20 +0600

    Banner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,14 @@
 import React from "react";
 
+// Import Components
+import Banner from "../components/Banner";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="min-h-[1800px]">
+      <Banner />
+    </section>
+  );
 };
 
 export default Home;
