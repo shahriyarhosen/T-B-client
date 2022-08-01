d2bd0d351654322cff5f780f051539be790d47a4
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-08-01 16:09:59 +0600

    react-router-dom install & setup done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
+    <section>
+      <Routes>
+        <Route to="" element=""></Route>
+      </Routes>
       <h1 className="text-7xl text-primary">React app</h1>
-    </div>
+    </section>
   );
 };
 
