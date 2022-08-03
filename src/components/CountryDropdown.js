716c37e98a3a7a1554848287af2502ad4ff4617d
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-08-03 09:43:05 +0600

    coustom css add

--- src/components/CountryDropdown.js
+++ src/components/CountryDropdown.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useContext, useState } from "react";
+
+// import icons
+import { RiMapPinLine, RiArrowDownSLine, RiArrowUpSLine } from "react-icons/ri";
+
+// import headless ui
+import { Menu } from "@headlessui/react";
+
+// import house context
+import { HouseContext } from "./HouseContext";
 
 const CountryDropdown = () => {
-  return <div>CountryDropdown</div>;
+  const { country, setCountry, countries } = useContext(HouseContext);
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  console.log(country);
+  return <Menu as="div" >CountryDropdown</Menu>;
 };
 
 export default CountryDropdown;
