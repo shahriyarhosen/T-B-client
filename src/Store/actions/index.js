96e7697aa24e197ae73e414e54759aec88bff7fc
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-08-06 18:06:04 +0600

    reducer setup done

---
+++ src/Store/actions/index.js
@@ -0,0 +1,7 @@
+export const action1 = (emm) => {
+  return { type: "action1", payload: emm };
+};
+
+export const action2 = () => {
+  return { type: "action2" };
+};
---
+++ src/Store/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import reducerFunction from "./upDown";
+
+const rootReducer = combineReducers({ reducerFunction });
+
+export default rootReducer;
